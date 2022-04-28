cbbc331cedcff2d56e174d1afa7553c4b6258fb6
Amanuel Mulugeta <91116670+Emanuel-Afro@users.noreply.github.com>
2022-04-28 19:12:42 +0300

    This is utility file deals with the error handling

---
+++ __utils__/__utils__.js
@@ -0,0 +1,27 @@
+const { send } = require("express/lib/response")
+
+const SendErrProduction = (err,res)=>{
+    res.status(err.statusCode).json({
+        status:er.status,
+        message:err.message
+    })
+}
+const SendErrDevelopmet = (err,res)=>{
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message,
+        error:err,
+        stack:err.stack,
+    })
+}
+module.exports = catchAsync = fn=>{
+    return(req,res,next)=>{
+        fn(req,res,next).catch(next);
+if(process.env.NODE_ENV === 'developement'){
+    SendErrDevelopmet(err,res)
+}
+else if(process.env.NODE_ENV === 'production'){
+    SendErrProduction(err,res)
+}
+    }
+}
