2dde25463946e61df7be280544a9337f81e907b9
Amanuel Mulugeta <91116670+Emanuel-Afro@users.noreply.github.com>
2022-04-28 19:57:16 +0300

    These files use to post new work by students.
    The post work will help students to post new jobs with their specific need. It has a route handler and route controller. It has its own schema and the posted job will be stored in the database and it will be sent to the mentors database. Also have pagination,sorting and filtering functionalities.

---
+++ Post_work/Schema/PostWorkModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const PostWorkSchema = new mongoose.Schema({
+    Student_First_Name:{
+        type: String,
+        required:[true,'A Student must have a FirstName'],
+        maxlength:[40,'A Student FirstName must have less than or equal then 40 characters'],
+      //  minlength:[10,'A Student LastName must have more than or equal than 10 characters']
+    },
+    Student_Last_Name:{
+        type: String,
+        required:[true,'A Student must have a LastName'],
+        maxlength:[40,'A Student LastName must have less than or equal then 40 characters '],
+     //   minlength:[10,'A Student LastName must have more than or equal than 10 characters']
+        
+    },
+   Grade_level:{
+        type:Number,
+        required:[true,'Student must submit Grade Level']
+    },
+  Minimum_Price:{
+      type:Number,
+      required:[true,'Minimum Price is Mandatory']
+  },
+  Maximum_Price:{
+      type:Number,
+      required:[true,'Maximum Price is Mandatory']
+  },
+  Subjects:{
+      type:String,
+      required:[true,'Define the subjects you want to study']
+  },
+  Available_Time:{
+      type:Number,
+      required:[true,'Select the time you are available']
+  },
+  Experiance:{
+    type:String,
+    required:[true,'Enter your Experiance level']
+  },
+  Location:{
+      type:String,
+      required:[true,'You have to enter your location'],
+
+  }
+})
+const PostWork = mongoose.model('PostWork',PostWorkSchema)
+module.exports = PostWork;
