49a6b36360a283ee7e2523e9a1a98aa19234a95d
Amanuel Mulugeta <91116670+Emanuel-Afro@users.noreply.github.com>
2022-05-21 21:04:56 +0300

    Delete Student_Details directory

--- Student_Details/Routes/StudentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const StudentController = require('../../Student_Details/Controller/StudentController')
-const MentorController = require('../../../Mentors/Controllers/MentorController')
-const router = express.Router()
-router.route('/').get(StudentController.GetAllMentors);
-router.route('/:id').get(StudentController.GetStudent)
-router.route('/').post(StudentController.CreateStudents);
-router.route('/:id').patch(StudentController.UpdateStudents);
-module.exports = router;
